a82cfc6707fcce59741c501b0aed112a13706c9b
Riccardo <curnis.riccardo.06@itisdalmine.edu.it>
2023-06-13 09:30:42 +0200

    Inizio Lavoro

---
+++ AgaGym.py
@@ -0,0 +1,10 @@
+
+#Curnis Riccardo - Daniele Morgan - Medolago Simone - Panseri Gabriele - Ratti Gabriele - Tosques Simone
+#progetto summer camp
+#--------------Codice---------------
+
+#dichiaro i dizionari
+
+def p_input():
+    
+
