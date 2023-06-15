a7f1e7abfbba15f58853e1a96af36329fcfbb293
Riccardo <curnis.riccardo.06@itisdalmine.edu.it>
2023-06-16 00:47:55 +0200

    Pre-Final release

--- prova.py
+++ prova.py
@@ -10,7 +10,7 @@ from functools import partial
 window = tk.Tk()
 window.geometry("600x600")
 window.resizable(True, True)
-window.title("AgaGym Software")
+window.title("AGym Software")
 
 Titolo = "AgaGym"
 titolo_output = tk.Label(window, text=Titolo, fg="red", font=("Courier", 30,"bold"))
@@ -662,18 +662,266 @@ def visualizza():
     testo = "Menu Visualizza:"
     text4_output = tk.Label(window, text=testo, fg="red", font=("Courier", 30,"bold"))
     text4_output.grid(row=0, column=1, padx=150)
-    indietro_button = tk.Button(window,text="Indietro",command=lambda:[indietro_button.grid_remove(),text4_output.grid_remove(),inizio(), exit_button.grid(row=6, column=1)])
-    indietro_button.grid(row=3, column=1)
+    indietro_button = tk.Button(window,text="Indietro",command=indietroB)
+    indietro_button.grid(row=5, column=1)
+    text1=Label(window, text="Inserisci il codice fiscale formato A A A A A A N N A N N A N N N A: ")
+    sbagliato2=Label(window,text="Errore, Codice fiscale non valido!", fg="red")
+    def codicefiscale():
+        ctrl=0
+        print("Ciao Siamo qyi")
+        global cod_fis
+        cod_fis=scrivi2.get()
+        cod_fis=cod_fis.upper()
+        if (len(cod_fis))!=16:
+            ctrl=1
+            print("Errore! Codice Fiscale non valido! 1 \n")
+            sbagliato2.grid(row=4, column=1)
+        if ctrl==0:
+            
+            for i in range(6):
+                
+                if cod_fis[i]not in lettere and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!2 \n")
+                    sbagliato2.grid(row=4, column=1)
+            for i in range (6,8):
+                if cod_fis[i]not in numeri and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!3 \n")
+                    sbagliato2.grid(row=4, column=1)
+            if cod_fis[8] not in lettere and ctrl!=1:
+                ctrl=1
+                print("Errore! Codice Fiscale non valido!4 \n")
+                sbagliato2.grid(row=4, column=1)
+            for i in range (9,11):
+                if cod_fis[i] not in numeri and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!5 \n")
+                    sbagliato2.grid(row=4, column=1)
+            if cod_fis[11] not in lettere and ctrl!=1:
+                ctrl=1
+                print("Errore! Codice Fiscale non valido!6 \n")
+                sbagliato2.grid(row=4, column=1)
+            for i in range (12,15):
+                if cod_fis[i] not in numeri and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!7 \n")
+                    sbagliato2.grid(row=4, column=1)
+            if cod_fis[15] not in lettere and ctrl!=1:
+                ctrl=1
+                print("Errore! Codice Fiscale non valido8 ! \n")
+                sbagliato2.grid(row=4, column=1)
+            if ctrl==0:
+                print("Siamo qui")
+                sbagliato2.grid_remove()
+                miofile=open("AgaGym.txt","r")
+                buffer=miofile.read()
+                buffer=buffer.split("\n")
+                
+                miofile.close()
+                listaUtenti=buffer
+                ctrll=0
+                for i in range(len(listaUtenti)-1):
+                    uten=listaUtenti[i].split("|")
+                    codf=uten[2]
+                    print(codf)
+                    if codf==cod_fis:
+                        ctrll=1
+                        break
+                    else:
+                        ctrll=0
+                if ctrll==1:
+                    for widgets in window.winfo_children():
+                        widgets.destroy()
+                    testo = "Visualizza:"
+                    text4_output = tk.Label(window, text=testo, fg="red", font=("Courier", 30,"bold"))
+                    text4_output.grid(row=0, column=1, padx=150)
+                    testoNome=f"Nome: {uten[0]}"
+                    testoCogone=f"Cognome: {uten[1]}"
+                    testoCod_Fiscale=f"Codice Fiscale: {uten[2]}"
+                    testoData=f"Data:{uten[3]}"
+                    testoAbb=f"Abbonamento:{uten[4]}"
+                    labelNome=Label(window, text=testoNome)
+                    labelCogone=Label(window, text=testoCogone)
+                    labelCodiceFis=Label(window, text=testoCod_Fiscale)
+                    labelData=Label(window, text=testoData)
+                    labelAbb=Label(window, text=testoAbb)
+                    labelNome.grid(row=1, column=1)
+                    labelCogone.grid(row=2,column=1)
+                    labelCodiceFis.grid(row=3, column=1)
+                    labelData.grid(row=4, column=1)
+                    labelAbb.grid(row=5, column=1)
+                    tornapatras=Button(window, text="Torna Indietro", command=lambda:[cancellatutto(), visualizza()]) 
+                    tornapatras.grid(row=6, column=1)                                                                                                           
+                else:
+                    sbagliato2.grid(row=4, column=1)
+                        
+
+
+    text1.grid(row=1, column=1)
+    scrivi2=Entry(window)
+    invio=Button(window, text="Invia", command=codicefiscale)
+    scrivi2.grid(row=2, column=1)
+    invio.grid(row=3, column=1)
+
+    
 
 def verifica():
     testo = "Menu Verifica:"
     text4_output = tk.Label(window, text=testo, fg="red", font=("Courier", 30,"bold"))
     text4_output.grid(row=0, column=1, padx=150)
     indietro_button = tk.Button(window,text="Indietro",command=lambda:[indietro_button.grid_remove(),text4_output.grid_remove(),inizio(), exit_button.grid(row=6, column=1)])
-    indietro_button.grid(row=3, column=1)
-
-
-
+    indietro_button.grid(row=190, column=1)
+    text5=Label(window, text="Inserisci il codice Fiscale: formato: A A A A A A N N A N N A N N N A" )
+    text5.grid(row=1, column=1)
+    sbagliato2=Label(window,text="Errore, Codice fiscale non valido!", fg="red")
+    
+    def codicefiscale():
+        ctrl=0
+        print("Ciao Siamo qyi")
+        global cod_fis
+        cod_fis=scrivi3.get()
+        cod_fis=cod_fis.upper()
+        if (len(cod_fis))!=16:
+            ctrl=1
+            print("Errore! Codice Fiscale non valido! 1 \n")
+            sbagliato2.grid(row=4, column=1)
+        if ctrl==0:
+            
+            for i in range(6):
+                
+                if cod_fis[i]not in lettere and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!2 \n")
+                    sbagliato2.grid(row=4, column=1)
+            for i in range (6,8):
+                if cod_fis[i]not in numeri and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!3 \n")
+                    sbagliato2.grid(row=4, column=1)
+            if cod_fis[8] not in lettere and ctrl!=1:
+                ctrl=1
+                print("Errore! Codice Fiscale non valido!4 \n")
+                sbagliato2.grid(row=4, column=1)
+            for i in range (9,11):
+                if cod_fis[i] not in numeri and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!5 \n")
+                    sbagliato2.grid(row=4, column=1)
+            if cod_fis[11] not in lettere and ctrl!=1:
+                ctrl=1
+                print("Errore! Codice Fiscale non valido!6 \n")
+                sbagliato2.grid(row=4, column=1)
+            for i in range (12,15):
+                if cod_fis[i] not in numeri and ctrl!=1:
+                    ctrl=1
+                    print("Errore! Codice Fiscale non valido!7 \n")
+                    sbagliato2.grid(row=4, column=1)
+            if cod_fis[15] not in lettere and ctrl!=1:
+                ctrl=1
+                print("Errore! Codice Fiscale non valido8 ! \n")
+                sbagliato2.grid(row=4, column=1)
+            if ctrl==0:
+                print("Siamo qui")
+                sbagliato2.grid_remove()
+                miofile=open("AgaGym.txt","r")
+                buffer=miofile.read()
+                buffer=buffer.split("\n")
+                
+                miofile.close()
+                listaUtenti=buffer
+                ctrll=0
+                for i in range(len(listaUtenti)-1):
+                    uten=listaUtenti[i].split("|")
+                    codf=uten[2]
+                    print(codf)
+                    if codf==cod_fis:
+                        ctrll=1
+                        break
+                    else:
+                        ctrll=0
+                if ctrll==0:
+                    sbagliato2.grid(row=4, column=1)
+                else:
+                    print("OLE")
+                    cliente=listaUtenti[i]
+                    cliente=cliente.split("|")
+                    data=cliente[3]
+                    data=data.split("-")
+                    giorno=data[2]
+                    n_1=giorno[0]
+                    n_2=giorno[1]
+                    giorno=n_1+n_2
+                    giorno=int(giorno)
+                    data.pop()
+                    anno=data[0]
+                    anno=int(anno)
+                    mese=data[1]
+                    mese=int(mese)
+                    if cliente[4]=="Annuale":
+                        anno=anno+1
+                    if cliente[4]=="Semestrale":
+                        mese=mese+6
+                        if mese>12:
+                            anno=anno+1
+                            c=mese-12
+                            mese=c
+                    if cliente[4]=="Trimestrale":
+                        mese=mese+3
+                        if mese>12:
+                            anno=anno+1
+                            c=mese-12
+                            mese=c
+                    controllomese=int(datetime.datetime.now().month)
+                    controlloanno=int(datetime.datetime.now().year)
+                    controllogiorno=int(datetime.datetime.now().day)
+                    Testo="Data di scadenza: {:}/{:}/{:} ".format(anno, mese, giorno)
+                    Testog=Testo+"(Scaduto)"
+                    Scaduto=Label(window, text=Testog, fg="red")
+                    NonScaduto=Label(window, text=Testo, fg="green")
+                    testoNome=f"Nome: {cliente[0]}"
+                    testoCogone=f"Cognome: {cliente[1]}"
+                    testoCod_Fiscale=f"Codice Fiscale: {cliente[2]}"
+                    testoAbb=f"Abbonamento:{cliente[4]}"
+                    labelNome=Label(window, text=testoNome)
+                    labelCogone=Label(window, text=testoCogone)
+                    labelCodiceFis=Label(window, text=testoCod_Fiscale)
+                    labelAbb=Label(window, text=testoAbb)
+                    labelNome.grid(row=1, column=1)
+                    labelCogone.grid(row=2,column=1)
+                    labelCodiceFis.grid(row=3, column=1)
+                    labelAbb.grid(row=4, column=1)
+                    tornapatras=Button(window, text="Torna Indietro", command=lambda:[cancellatutto(), verifica()]) 
+                    tornapatras.grid(row=6, column=1)
+                    if controlloanno>anno:
+                        print("Scaduti")
+                        Scaduto.grid(row=5, column=1)
+                    elif controlloanno==anno:
+                        print("Scaduti")
+                        Scaduto.grid(row=5, column=1)
+                        if controllomese>mese:
+                            print("Scaduti")
+                            Scaduto.grid(row=5, column=1)
+                        elif controllomese==mese:
+                            print("Scaduti")
+                            Scaduto.grid(row=5, column=1)
+                            if controllogiorno>=giorno:
+                                print("Scaduti")
+                                Scaduto.grid(row=5 , column=1)
+                            else:
+                                print("No")
+                                NonScaduto.grid(row=5 , column=1)
+                        else:
+                            print("No")
+                            NonScaduto.grid(row=5 , column=1)
+                    else:
+                        print("No")
+                        NonScaduto.grid(row=5 , column=1)
+            
+    scrivi3=Entry(window)
+    scrivi3.grid(row=2, column=1)
+    invio1=Button(window, text="Invia", command=lambda:[codicefiscale(), scrivi3.grid_remove(), invio1.grid_remove(), indietro_button.grid_remove(),text5.grid_remove()])
+    invio1.grid(row=3, column=1)
 
     
 window.mainloop() 
